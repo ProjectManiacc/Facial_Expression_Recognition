8ab8851225bebf858eb8dc54ee3f116f5fa2ad8c
Piotrek Kluziok <piotrkluziok@gmail.com>
2023-02-25 20:50:13 +0100

    Completed project

--- model.py
+++ model.py
@@ -1,18 +1,17 @@
 from tensorflow.keras.models import model_from_json
 import numpy as np
 
-
-
 class FacialExpressionModel(object):
 
     EXPRESSIONS = ["Angry", "Disgust", "Fear", "Happy", "Neutral", "Sad", "Suprise"]
 
     def __init__(self, model_json_file, model_weights_file):
-        with open(model_json_file,"r") as json_file:
+        print(model_json_file)
+        with open(model_json_file, "r") as json_file:
             loaded_model_json = json_file.read()
-            self.loaded = model_from_json(loaded_model_json)
-
-        self.loaded_model.load_weight(model_weights_file)
+            self.loaded_model = model_from_json(loaded_model_json)
+        print(model_weights_file)
+        self.loaded_model.load_weights(model_weights_file)
         self.loaded_model.make_predict_function()
 
     def predict_expression(self, img):
